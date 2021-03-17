74de06874563e4f5254ac441a98e08351301a07f
fahadalsaedi <alsaedifahad58@gmail.com>
2021-03-17 04:33:44 +0300

    rating

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Product from './components/Product';
 import data from './data';
 // import Articles from './components/articles';
 // import axios from 'axios';
@@ -50,38 +51,7 @@ class App extends React.Component {
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product = {product}/>
             ))}
           </div>
         </div>
